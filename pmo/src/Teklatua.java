f47fd0b8adb9830dddc82474c75facf42cf2b8a4
Midoiaga <49227878+Midoiaga@users.noreply.github.com>
2019-04-15 08:37:23 +0200

    Add files via upload

---
+++ pmo/src/Teklatua.java
@@ -0,0 +1,21 @@
+package civilization;
+import java.util.Scanner;
+public class Teklatua {
+	private Scanner sc;
+	private static Teklatua nireTeklatua;
+
+	private Teklatua(){
+		this.sc=new Scanner(System.in);
+	}
+	public static Teklatua getNireTeklatua(){
+		if(nireTeklatua==null){
+			nireTeklatua=new Teklatua();
+		}
+		return nireTeklatua;
+	}
+	public int irakurriZenb()throws NumberFormatException{
+		String sar=this.sc.nextLine();
+		int zenb =Integer.parseInt(sar);
+		return zenb;
+	}
+}
