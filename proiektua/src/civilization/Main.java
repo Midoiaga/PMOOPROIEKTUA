8d7030464a43b0a4fbc8624f21ccf58968cabdfc
jrenteria003 <jrenteria003@ikasle.ehu.eus>
2019-04-24 15:23:05 +0200

    Main.java sortu eta hasierako estruktura eman

---
+++ proiektua/src/civilization/Main.java
@@ -0,0 +1,17 @@
+package civilization;
+
+public int Main 
+{
+	boolean partida = true;
+	while(partida)
+	{
+		Civilization.txanda;
+		if(Civilization.amaituDa)
+		{
+			partida = false;
+		}
+	}
+
+	return 0;
+
+}
