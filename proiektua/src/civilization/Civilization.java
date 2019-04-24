3aef32e8625aa4afeaff865082c92d555a9b0ad8
jrenteria003 <jrenteria003@ikasle.ehu.eus>
2019-04-24 17:27:21 +0200

    Civilization klasean partida eta amaitu metodoak garatu

--- proiektua/src/civilization/Civilization.java
+++ proiektua/src/civilization/Civilization.java
@@ -2,37 +2,35 @@ package civilization;
 
 public class Civilization {
 
-	//acab
 	private Jokalaria[] jokalariak;
-	private Hiria[] hiriak;
 	
 	private static Civilization nirePartida = null;
 	
 	private Civilization() {
-		//eraik
+		
 		this.jokalariak[0] = new Jokalaria("");
 		this.jokalariak[1] = new Jokalaria("");
 	}
 	
 	public static Civilization getNirePartida() {
-		//singleton
 		if(nirePartida==null) {
 			nirePartida = new Civilization();
 		}
 		
 		return nirePartida;
 	}
-	
-	public void txanda() {
-		//TODO jokalarien arabera aldatu
-		this.jokalariak[0].txanda();
-		atakeak();
-		this.jokalariak[1].txanda();
-		atakeak();
+	public void partida(){
+		while(this.amaitu()==false){
+			this.jokalariak[0].txanda();
+			this.jokalariak[1].txanda();
+		}
+		
 	}
-	
-	
-	private void atakeak() {
-		//TODO ???
+	private boolean amaitu(){
+		boolean emaitza=false;
+		if(this.jokalariak[0].hiriKop()==0||this.jokalariak[1].hiriKop()==0){
+			emaitza=true;
+		}
+		return emaitza;
 	}
 }
