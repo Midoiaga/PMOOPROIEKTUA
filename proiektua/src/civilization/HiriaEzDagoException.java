6d930e150d8996b5c54efaaa33f38dc3aaa64d07
jrenteria003 <jrenteria003@ikasle.ehu.eus>
2019-05-01 18:02:03 +0200

    fitxategi berriak

---
+++ proiektua/src/civilization/HiriaEzDagoException.java
@@ -0,0 +1,12 @@
+package civilization;
+
+public class HiriaEzDagoException extends Exception {
+	
+	public HiriaEzDagoException() {
+		super();
+	}
+	
+	public void printStackTrace() {
+		System.out.println("Sartu duzun aukera ez da existitzen, bezte bat sartu :");
+	}
+}
