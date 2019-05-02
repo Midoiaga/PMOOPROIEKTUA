224e9d35810d0135e9494ba5e6c31a7d0e43e16b
jrenteria003 <jrenteria003@ikasle.ehu.eus>
2019-05-02 17:58:17 +0200

    Aukerak.java klasea igo, ahaztu zitzaidan lehengoan :D

---
+++ proiektua/src/civilization/Aukerak.java
@@ -0,0 +1,44 @@
+package civilization;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Aukerak {
+	
+	private static Aukerak nireAukerak = new Aukerak();
+	
+	private Aukerak() {
+		
+	}
+	
+	public static Aukerak getAukerak() {
+		return nireAukerak;
+	}
+	
+	public ArrayList<String> aukerakFiltro(String pFiltroa){
+		ArrayList<String> emaitza = new ArrayList<String>();
+		String fitxategia = Aukerak.class.getResource("Aukerak.txt").getFile();
+        String line = "";
+        try (BufferedReader br1 = new BufferedReader(new FileReader(fitxategia))) {
+        	
+            while ((line = br1.readLine()) != null) {
+                if(line.equals(pFiltroa)) {
+                	while (line != null&&line.equals(pFiltroa)) {
+                		line = br1.readLine();
+                		emaitza.add(line);
+                	}
+                	return emaitza;
+                }               
+            }
+            return null;
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+	}
+	
+	
+}
