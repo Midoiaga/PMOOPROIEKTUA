4a59fb59d9ef76d3c9b01e1618554ec08367af77
xxxrobotin <48926393+xxxrobotin@users.noreply.github.com>
2019-05-07 23:18:08 +0200

    Update and rename proiektua/src/civilization/ListaEraikinak.java to  proiektua/ src/ civilization/ListaEraikinak.java

--- proiektua/ src/ civilization/ListaEraikinak.java
+++ proiektua/ src/ civilization/ListaEraikinak.java
@@ -25,8 +25,37 @@ public class ListaEraikinak {
 		while(i<bezteEraikinak.size()) {
 			bezteEraikinak.get(i).printKostua();
 		}
+package civilization;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class ListaEraikinak {
+	
+	//atrib
+	private ArrayList<Eraikina> dituzunEraikinak;
+	private ArrayList<Eraikina> bezteEraikinak;
+	
+	public ListaEraikinak() {
+		this.dituzunEraikinak = new ArrayList<Eraikina>();
+		this.bezteEraikinak = new ArrayList<Eraikina>();
+		bezteEraikinak.add(new Eraikina("Zerratokia",100,100,50));
+		bezteEraikinak.add(new Eraikina("Harrobia",100,100,50));
+		bezteEraikinak.add(new Eraikina("Burdinola",100,100,50));
+		bezteEraikinak.add(new Eraikina("Urre Mina",100,100,50));
+	}
+	
+	public void printFalta() {
+		//zein eraikinak eraiki dezakeen jokalariak
+		System.out.println("Eta eraikin hauek eraiki ditzazkezu:");
+		int i = 0;
+		while(i<bezteEraikinak.size()) {
+			bezteEraikinak.get(i).printKostua();
+			i++;
+		}
 		System.out.println("Aukeratu bat:");
 		
+	
 	}
 	
 	
@@ -45,16 +74,7 @@ public class ListaEraikinak {
 		return this.dituzunEraikinak.iterator();
 	}
 
-	public Eraikina bilatuIzenez(String pIzena) {
-		Iterator<Eraikina> itr = this.getIteradorea();
-		Eraikina e = null;
-		while(itr.hasNext()) {
-			e = itr.next();
-			if(e.izenBeraDu(pIzena))return e;
-		}
-		return null;
-	}
-
+	
 	public void gehitu(Eraikina pEraikina) {
 		this.dituzunEraikinak.add(pEraikina);
 		this.bezteEraikinak.remove(pEraikina);
@@ -77,8 +97,18 @@ public class ListaEraikinak {
 		int i = 0;
 		while(i<bezteEraikinak.size()) {
 			falta.add(bezteEraikinak.get(i).getIzena());
+			i++;
 		}
 		return falta;
+	}
+
+	public Eraikina bilaturaikina(String aukera) {
+		for(Eraikina e : this.bezteEraikinak) {
+			if(e.izenBeraDu(aukera)) {
+				return e;
+			}
+		}
+		return null;
 	}
 	
 }
